828ece11a9ff2765d2ea100b1ae5b512ad78bc8f
jmlua <61339018+jmlua@users.noreply.github.com>
2020-04-05 21:31:29 +0800

    Update PropertyTest.java

--- src/PropertyTest.java
+++ src/PropertyTest.java
@@ -8,11 +8,15 @@ class PropertyTest {
 	void testToString() {
 		Property p1 = new Property(1992, 3, "Serangoon", "3", 98.2, 94, 288800.0);
 		assertEquals(p1.toString(), "[1992, 3, SERANGOON, 3, 98.2, 94, 288800.0]");
+		Property p2 = new Property(2005, 2, "Tampines", "2", 65.0, 72, 123500.0);
+		assertEquals(p2.toString(), "[2005, 2, TAMPINES, 2, 65.0, 72, 123500.0]");
 	}
 
 	@Test
 	void testProperty() {
-		Property p1 = new Property(2001, 5, "Ang Mo Kio", "5", 65.3, 67, 600000.0);
+		Property p2 = new Property(2001, 5, "Ang Mo Kio", "5", 65.3, 67, 600000.0);
+		Property p3 = new Property(1967, 12, "Punggol", "4", 76.4, 52, 140000.0);
+
 	}
 
 	@Test
