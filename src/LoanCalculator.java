bcd80046a6b9dd681adc54c519f07d03fd27ded8
jmlua <61339018+jmlua@users.noreply.github.com>
2020-04-20 22:24:12 +0800

    Created LoanCalculator and CalculatorController classes.
    Also updated layout for loan calculator in GUI

---
+++ src/LoanCalculator.java
@@ -0,0 +1,41 @@
+import java.math.*;
+
+/**
+ * This LoanCalculator class computes the monthly installment for the user
+ */
+
+public class LoanCalculator {
+
+	long price;
+	double annualRate;
+	int year;
+	
+	/**
+	 * This class calculates the average monthly mortgage installment  
+	 * given a flat price, interest rate (in percent) and the repayment period (in years)
+	 * using the PMT formula
+	 */
+	
+	public static String getInstallment(long price, double annualRate, int year) {
+		
+		//converts the interest rate to a float between 0 to 1
+		
+		double monthlyRate = (annualRate / 12) / 100;		
+		
+		//number of payments = num of years * num of months in a year
+		int numOfPayments = 12 * year;
+		
+		double compoundInterest = Math.pow((1 + monthlyRate), numOfPayments);
+		
+		//implement PMT formula to calculate monthly interest
+		double installment = price * ( monthlyRate * compoundInterest / (compoundInterest - 1)) ;
+		
+		return String.format("$%.2f", installment); 
+	}
+	
+	
+	public static void main(String[] args) {
+		System.out.println(LoanCalculator.getInstallment(200000, 2.6, 10));
+	}
+	
+}
