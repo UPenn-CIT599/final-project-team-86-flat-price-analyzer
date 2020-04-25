449462dcdba302502916f2ce2cea10e958756225
jmlua <61339018+jmlua@users.noreply.github.com>
2020-04-25 17:45:30 +0800

    Update CalculatorController.java

--- src/CalculatorController.java
+++ src/CalculatorController.java
@@ -23,7 +23,7 @@ public class CalculatorController {
 	@FXML
     public void initialize() {
 		
-		myProperties = PropertyReader.readFile("jan2019_dec2019.csv", true);
+		myProperties = PropertyReader.readFile("resale-prices-2005.csv", true);
 		
 		// initialize elements in various tabs
 		
